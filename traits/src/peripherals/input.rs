7352ddf92bfef1bbf46e84c8fefbc67660683a8f
Rahul Butani <rrbutani@users.noreply.github.com>
2022-06-09 17:43:00 -0700

    peripherals: add some TODOs for the `Input` trait
    (extracted from 797cdfae94aa6e80b0333eb112963c0173287f34)

--- traits/src/peripherals/input.rs
+++ traits/src/peripherals/input.rs
@@ -16,6 +16,15 @@ pub trait Input {
     //
     // Must use interior mutability.
     fn read_data(&self) -> Result<u8, InputError>;
+    // TODO: why not allow ^ to take `&mut self`?
+
+    // TODO: maybe introduce a "no perturb read" function?
+    // and an equivalent abstraction in `mem_mapped`?
+    //
+    // or just: have some mem_mapped devices have a "stateful_read = true" field
+    // which causes reads of them to emit
+    // 0s (or a "cannot read" error; we'd need to modify Control to support this but I think this is the way to go!)
+
 
     // this is a separate method than `read_data` because for interrupts we want
     // to be able to ask if there is any pending data without actually
@@ -29,9 +38,45 @@ pub trait Input {
     fn interrupts_enabled(&self) -> bool;
 }
 
+// TODO: have something like this in stubs for this module
+
+/*
+struct AlwaysHasData<const D: u8> {
+    interrupt_enabled: bool,
+}
+   impl<const D: u8> Input for AlwaysHasData<D> {
+    fn read_data(&mut self) -> Result<u8, InputError> {
+        Ok(D)
+    }
+    fn current_data_unread(&self) -> bool {
+        true
+    }
+    fn register_interrupt_flag(&mut self,flag: & 'a AtomicBool) {
+        todo!()
+    }
+    fn interrupt_occurred(&self) -> bool {
+        true
+    }
+    fn reset_interrupt_flag(&mut self,) {
+        /* we always have more data ready so we're always producing another interrupt, if interrupts are enabled */
+    }
+    fn set_interrupt_enable_bit(&mut self,bit:bool) {
+        self.interrupt_enabled = bit;
+    }
+    fn interrupts_enabled(&self) -> bool {
+        self.interrupt_enabled
+    }
+ */
+// TODO: provide embedded_hal based Input/Output impls? (not multiplexed)
+
+// TODO: pretty sure that if you don't read KBDR in your input interrupt ISR
+// the interrupt will keep firing (i.e. the ready bit will remain high).
+//
+// should test this against lc3tools
+
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub enum InputError {
-    NonUnicodeCharacter(u8),
+    NonUnicodeCharacter(u8), // TODO: should this be a u32?
     IoError,
     NoDataAvailable,
 }
