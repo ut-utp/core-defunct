a34bcb61f07ac8db5f49b43b86a9e38f8ffb362e
Rahul Butani <rrbutani@users.noreply.github.com>
2022-08-08 15:21:14 -0700

    dev-support: tweaks for `GenericPwm`

--- device-support/src/peripherals/pwm.rs
+++ device-support/src/peripherals/pwm.rs
@@ -1,8 +1,8 @@
-use lc3_traits::peripherals::pwm::{Pwm as lc3_pwm, *};
-use embedded_hal::{Pwm as hal_pwm, PwmPin as hal_pwm_pin};
-use core::marker::PhantomData;
 use core::cell::RefCell;
+use core::marker::PhantomData;
 use embedded_hal::prelude::_embedded_hal_PwmPin;
+use embedded_hal::{PwmPin as hal_pwm_pin};
+use lc3_traits::peripherals::pwm::{Pwm as lc3_pwm, *};
 
 // Embedded hal; offers the Pwm and PwmPin trait, but it looks like
 // PwmPin and Pwm hal traits don't really have anything to do with each other and are just both
@@ -14,30 +14,33 @@ use embedded_hal::prelude::_embedded_hal_PwmPin;
 // PwmPin is also the more mature trait as the hal Pwm trait is marked unproven.
 // Only drawback is the TM4C currently doesn't have an implementation for PwmPin (it has one for Pwm) but that is ok. TODO: Make PwmPin impl for TM4C
 
- pub struct generic_pwm_unit<T, U>
- where T: hal_pwm_pin + _embedded_hal_PwmPin<Duty = U>,
-       U: From<u16> + Into<u16> //adding this trait bound on duty to allow converting to integer form for our platform use. Similar approach as ADC
+pub struct GenericPwm<T, U>
+where
+    T: hal_pwm_pin + _embedded_hal_PwmPin<Duty = U>,
+    U: From<u16> + Into<u16>, //adding this trait bound on duty to allow converting to integer form for our platform use. Similar approach as ADC
 {
     hal_pins: RefCell<PwmPinArr<T>>,
     pin_states: PwmPinArr<PwmState>,
     phantom: PhantomData<U>,
- }
-
-impl <T, U> Default for generic_pwm_unit<T, U>
-where T: hal_pwm_pin<Duty = U>,
-      U: From<u16> + Into<u16>,
-{
-	fn default() -> Self{
-		unimplemented!()
-	}
 }
 
-impl <T, U> generic_pwm_unit<T, U>
-where T: hal_pwm_pin<Duty = U>,
-      U: From<u16> + Into<u16>,
+impl<T, U> Default for GenericPwm<T, U>
+where
+    T: hal_pwm_pin<Duty = U>,
+    U: From<u16> + Into<u16>,
 {
-    fn new(pwm_device_pins: PwmPinArr<T>) -> Self{
-        Self{
+    fn default() -> Self {
+        unimplemented!()
+    }
+}
+
+impl<T, U> GenericPwm<T, U>
+where
+    T: hal_pwm_pin<Duty = U>,
+    U: From<u16> + Into<u16>,
+{
+    pub fn new(pwm_device_pins: PwmPinArr<T>) -> Self {
+        Self {
             hal_pins: RefCell::new(pwm_device_pins),
             pin_states: PwmPinArr([PwmState::Disabled; PwmPin::NUM_PINS]),
             phantom: PhantomData,
@@ -45,16 +48,18 @@ where T: hal_pwm_pin<Duty = U>,
     }
 }
 
-impl <T, U> lc3_pwm for generic_pwm_unit<T, U>
-where T: hal_pwm_pin + _embedded_hal_PwmPin<Duty = U>,
-      U: From<u16> + Into<u16>,
- {
-    fn set_state(&mut self, pin: PwmPin, state: PwmState){
+impl<T, U> lc3_pwm for GenericPwm<T, U>
+where
+    T: hal_pwm_pin + _embedded_hal_PwmPin<Duty = U>,
+    U: From<u16> + Into<u16>,
+{
+    fn set_state(&mut self, pin: PwmPin, state: PwmState) {
         let mut hal_pins = self.hal_pins.borrow_mut();
-        match state{
+        match state {
             PwmState::Enabled(duty) => {
                 hal_pins[pin].enable();
-                hal_pins[pin].set_duty(U::from(core::num::NonZeroU8::get(duty) as u16));
+                hal_pins[pin]
+                    .set_duty(U::from(core::num::NonZeroU8::get(duty) as u16));
             }
             PwmState::Disabled => {
                 hal_pins[pin].disable();
@@ -62,8 +67,8 @@ where T: hal_pwm_pin + _embedded_hal_PwmPin<Duty = U>,
         }
         self.pin_states[pin] = state;
     }
-    fn get_state(&self, pin: PwmPin) -> PwmState{
-    	self.pin_states[pin]
+    fn get_state(&self, pin: PwmPin) -> PwmState {
+        self.pin_states[pin]
     }
     #[inline]
     fn get_states(&self) -> PwmPinArr<PwmState> {
@@ -80,17 +85,22 @@ where T: hal_pwm_pin + _embedded_hal_PwmPin<Duty = U>,
     //For ADC, couldn't read a disabled pin for instance, but since there is no
     //error checking here, just enable and set a pin of it is disabled.
     //And just set duty to 1 if 0 to avoid crashing on unwrap
-    fn set_duty_cycle(&mut self, pin: PwmPin, duty_cycle: PwmDutyCycle){
+    fn set_duty_cycle(&mut self, pin: PwmPin, duty_cycle: PwmDutyCycle) {
         let mut duty_cycle_temp = duty_cycle;
-        if(duty_cycle_temp == 0){
+        if duty_cycle_temp == 0 {
             duty_cycle_temp = 1;
         }
-    	self.set_state(pin, PwmState::Enabled(core::num::NonZeroU8::new(duty_cycle_temp).unwrap()));
+        self.set_state(
+            pin,
+            PwmState::Enabled(
+                core::num::NonZeroU8::new(duty_cycle_temp).unwrap(),
+            ),
+        );
     }
-    fn get_duty_cycle(&self, pin: PwmPin) -> PwmDutyCycle{
-    	let state = self.get_state(pin);
+    fn get_duty_cycle(&self, pin: PwmPin) -> PwmDutyCycle {
+        let state = self.get_state(pin);
         let mut duty_return = core::num::NonZeroU8::new(1).unwrap();
-        match state{
+        match state {
             PwmState::Enabled(duty) => {
                 duty_return = duty;
             }
